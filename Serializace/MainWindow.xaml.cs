8022d4c02ea85c8cfe1031d2871e667787c9ed59
22ib20_maly <22ib20_maly@U1-04>
2024-05-14 08:57:29 +0200

    Přidejte soubory projektu.

---
+++ Serializace/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Serializace
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string cesta = "Nastavení.xml";
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closing += (s, e) => Serializuj();
+
+            Loaded += (s, e) => Deserializuj();
+
+        }
+
+        void Serializuj()
+        {
+            Settings set = new()
+            {
+                Jedna = (bool)prvni.IsChecked,
+                Dva = (bool)druhy.IsChecked,
+                Tri = (bool)treti.IsChecked,
+            };
+
+            XmlSerializer writer = new XmlSerializer(typeof(Settings));
+            
+            using(StreamWriter sw = new StreamWriter(cesta))
+            {
+                writer.Serialize(sw, set);
+                sw.Close();
+
+            }
+        }
+
+        void Deserializuj()
+        {
+            XmlSerializer reader = new XmlSerializer(typeof (Settings));
+            Settings nastaveni = new Settings();
+            if(File.Exists(cesta))
+            {
+                using(StreamReader sr = new StreamReader(cesta)) 
+                {
+                   nastaveni = (Settings)reader.Deserialize(sr);
+                }
+            }
+
+            prvni.IsChecked = nastaveni.Jedna;
+            druhy.IsChecked = nastaveni.Dva;
+            treti.IsChecked = nastaveni.Tri;
+        }
+    }
+}
---
+++ Serializace/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializace
+{
+    public class Settings
+    {
+        public bool Jedna {  get; set; }
+        public bool Dva { get; set; }
+        public bool Tri { get; set; }
+    }
+}
